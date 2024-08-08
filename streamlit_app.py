08c6033f98801e5029b40a48fdb3f6d6f577a479
carmelkron <carmelk@mail.tau.ac.il>
2024-08-08 13:52:07 +0000

    little changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
+st.title("Counter-Narrative Evaluation")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    ":baloon: Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write("I'm Carmel and i changed this file. Can you see it?")
+
+st.write(pd.read_csv('cn_dataset_LLAMA.csv'))
