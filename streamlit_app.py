f5bfb64b9217dc55044be99731214d60e1996ef3
carmelkron <carmelk@mail.tau.ac.il>
2024-08-22 13:32:51 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,19 +6,34 @@ from streamlit_gsheets import GSheetsConnection
 
 # Set up the page
 st.set_page_config(layout="wide")
+custom_margins = """
+<style>
+    .main {
+        margin-left: 5%;  /* Adjust the margin-left as needed */
+        margin-right: 5%; /* Adjust the margin-right as needed */
+    }
+</style>
+"""
+st.markdown(custom_margins, unsafe_allow_html=True)
+
 # Initialize session state
 if 'random_base_claim' not in st.session_state:
     st.session_state.random_base_claim = None
     st.session_state.relevant_counter_narratives = None
     st.session_state.cn_pair = None
     st.session_state.selections = {}
+    st.session_state.question_count = 0
+    st.session_state.pending_updates = []
+
+# Set the number of questions before completion (can be changed as needed)
+TOTAL_QUESTIONS = 20
 
 # Load the data
 conn = st.connection("gsheets", type=GSheetsConnection)
 data = conn.read(worksheet="cn_dataset_LLAMA")
 df = pd.DataFrame(data)
 
-# Function to get a random base claim and its counter-narratives from two different propagnda techniques
+# Function to get a random base claim and its counter-narratives from two different propaganda techniques
 def get_random_claim_and_responses():
     random_base_claim = np.random.choice(df['base_claim'].unique())
     relevant_counter_narratives = df[df['base_claim'] == random_base_claim].reset_index(drop=True)
@@ -29,92 +44,106 @@ def get_random_claim_and_responses():
     return random_base_claim, relevant_counter_narratives, cn_pair
 
 # Function to update counter
-def update_counter(question_number, response_id):
-    col_name = f'q{question_number}_counter'
-    df[col_name] = pd.to_numeric(df[col_name], errors='coerce')
-    val = df[df['response_id'] == response_id][col_name].iloc[0]
-    df.loc[df['response_id'] == response_id, col_name] = val + 1
-    conn.update(worksheet="cn_dataset_LLAMA", data=df)
+def update_counter(question_number, response_ids):
+    for response_id in response_ids:
+        col_name = f'q{question_number}_counter'
+        df[col_name] = pd.to_numeric(df[col_name], errors='coerce')
+        val = df[df['response_id'] == response_id][col_name].iloc[0]
+        df.loc[df['response_id'] == response_id, col_name] = val + 1
+    st.session_state.pending_updates.extend([(question_number, response_id) for response_id in response_ids])
+
+# Function to apply pending updates to Google Sheets
+def apply_pending_updates():
+    if st.session_state.pending_updates:
+        for question, response_id in st.session_state.pending_updates:
+            col_name = f'q{question}_counter'
+            df[col_name] = pd.to_numeric(df[col_name], errors='coerce')
+            val = df[df['response_id'] == response_id][col_name].iloc[0]
+            df.loc[df['response_id'] == response_id, col_name] = val + 1
+        conn.update(worksheet="cn_dataset_LLAMA", data=df)
+        st.session_state.pending_updates = []
 
 # Main app
 def main():
-    if 'counter' not in st.session_state:
-        st.session_state.counter = 0
-    if st.session_state.random_base_claim is None or (st.session_state.current_question > 3):
+    if st.session_state.random_base_claim is None:
         st.session_state.random_base_claim, st.session_state.relevant_counter_narratives, st.session_state.cn_pair = get_random_claim_and_responses()
-        st.session_state.selections = {}
-        st.session_state.current_question = 1
-        st.session_state.pending_updates = []
+
+    if st.session_state.question_count >= TOTAL_QUESTIONS:
+        st.markdown("<h1 style='text-align: center;'>Thank you for your effort!</h1>", unsafe_allow_html=True)
+        apply_pending_updates()
+        return
+
     
-    st.markdown(f"<h5 style='text-align:center;'>Number of evaluations so far: {st.session_state.counter}</h5>", unsafe_allow_html=True)
-    st.markdown(f"<h2 style='text-align:center;'>Here is a claim that is often claimed by {st.session_state.relevant_counter_narratives.loc[0, 'claimed_by']}:</h2>", unsafe_allow_html=True)
-    st.markdown(f'### <p style="color:red; font-size:28px;">"{st.session_state.random_base_claim}"</p>', unsafe_allow_html=True)
+    example_narrative = st.session_state.relevant_counter_narratives.loc[0]
+    pro_what = "pro-Russian" if example_narrative['claimed_by'] == 'Russia' else "pro-Ukrainian"
     
-    # Define the three questions
+    st.markdown(f"<h4 style='text-align:center;'>Look at this {pro_what} claim</h4>", unsafe_allow_html=True)
+    st.markdown(f'<p style="color:red; text-align:center; font-size: 30px;">{st.session_state.random_base_claim}</p>', unsafe_allow_html=True)
+    
     questions = [
         "Which do you find more convincing as a rebuttal to the opinion displayed above, A or B?",
         "Which evokes stronger emotions, A or B?",
         "Which do you think the average social media user is more likely to find interesting to share (repost/retweet), A or B?"
     ]
     
-    # Display current question
-    q_num = st.session_state.current_question
-    if q_num <= 3:
-        st.markdown(f'### <p style="font-size:24px; text-align:center; font-weight:bold;"> {questions[q_num-1]} </p>', unsafe_allow_html=True)
+    q_num = st.session_state.question_count % 3
+    st.markdown(f'<h4 style="text-align:center; max-width: 60%; margin: 0 auto;">{questions[q_num]}</h4>', unsafe_allow_html=True)
+    
+    col1, col_middle, col2 = st.columns([15,1,15])
+    st.markdown("""
+    <style>                    
+    .equal-height-paragraph {
+        padding: 10px;
+        text-align: center;
+        font-size: 25px; 
+        border: 2px solid black;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: fit-content;
+        height: 275px;
+    }
+    """, unsafe_allow_html=True)
+
+    with col1:
+        st.markdown("<h2 style='text-align:center;'>A</h2>", unsafe_allow_html=True)
+        first_text = st.session_state.cn_pair.loc[0, 'response_text']
+        st.markdown(f'<p class="equal-height-paragraph">{first_text}</p>', unsafe_allow_html=True)
         
-        col1, col2 = st.columns(2)
+    with col2:
+        st.markdown("<h2 style='text-align:center;'>B</h2>", unsafe_allow_html=True)
+        second_text = st.session_state.cn_pair.loc[1, 'response_text']
+        st.markdown(f'<p class="equal-height-paragraph">{second_text}</p>', unsafe_allow_html=True)
         
-        # Get the length of both button texts, ensuring they are strings
-        text_a = str(st.session_state.cn_pair.loc[0, 'response_text'])
-        text_b = str(st.session_state.cn_pair.loc[1, 'response_text'])
-        max_length = max(len(text_a), len(text_b))
-        
-        # CSS to make buttons the same size
-        button_style = f"""
-        <style>
-        .stButton > button {{
-            width: 100%;
-            height: {max_length // 30}em;  # Adjust this calculation as needed
-            white-space: normal;
-            word-wrap: break-word;
-        }}
-        </style>
-        """
-        st.markdown(button_style, unsafe_allow_html=True)
-        
-        with col1:
-            st.markdown(f"<h5 style='text-align:center;'>A</h5>", unsafe_allow_html=True)
-            if st.button(text_a, key=f"q{q_num}_a"):
-                st.session_state.selections[q_num] = 'A'
-        
-        with col2:
-            st.markdown(f"<h5 style='text-align:center;'>B</h5>", unsafe_allow_html=True)
-            if st.button(text_b, key=f"q{q_num}_b"):
-                st.session_state.selections[q_num] = 'B'
-        
-        # Continue button below the two columns
-        _, col2, _ = st.columns([1,1,1])
-        with col2:
-            if st.button("Continue", key=f"continue_{q_num}"):
-                if q_num in st.session_state.selections:
-                    st.session_state.counter += 1
-                    selected_option = st.session_state.selections[q_num]
-                    response_id = st.session_state.cn_pair.loc[0 if selected_option == 'A' else 1, 'response_id']
-                    st.session_state.pending_updates.append((q_num, response_id))
-                    if q_num < 3:
-                        st.session_state.current_question += 1
-                        st.rerun()
-                    else:
-                        for question, response_id in st.session_state.pending_updates:
-                            update_counter(question, response_id)
-                        st.session_state.current_question = 4  # This will trigger a new claim on the next run
-                        st.rerun()
-                else:
-                    st.warning("Please select an option before continuing.")
-    else:
-        st.success("You've completed all questions for this claim. A new claim will be presented.")
-        st.session_state.random_base_claim = None  # Reset to trigger new claim generation
-        st.rerun()
+    col1, col2, col3, col4 = st.columns(4)
+    
+    with col1:
+        if st.button(":point_left: A is better", use_container_width=True):
+            update_counter(q_num + 1, [st.session_state.cn_pair.loc[0, 'response_id']])
+            st.session_state.selections[q_num] = 'A'
+    
+    with col2:
+        if st.button(":point_right: B is better", use_container_width=True):
+            update_counter(q_num + 1, [st.session_state.cn_pair.loc[1, 'response_id']])
+            st.session_state.selections[q_num] = 'B'
+    
+    with col3:
+        if st.button(":handshake: Tie", use_container_width=True):
+            update_counter(q_num + 1, [st.session_state.cn_pair.loc[0, 'response_id'], st.session_state.cn_pair.loc[1, 'response_id']])
+            st.session_state.selections[q_num] = 'Tie'
+    
+    with col4:
+        if st.button(":-1: Both are bad", use_container_width=True):
+            st.session_state.selections[q_num] = 'Bad'
+    
+    _, col, _ = st.columns([1,1,1])
+    with col:
+        if st.button("Next question →", use_container_width=True):
+            st.session_state.question_count += 1
+            if st.session_state.question_count % 3 == 0:
+                st.session_state.random_base_claim = None
+                apply_pending_updates()
+            st.rerun()
 
 if __name__ == "__main__":
     main()
