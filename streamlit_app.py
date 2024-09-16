5115f3563b7b400a5f23360c70d16f8d1052a9ce
carmelkron <carmelk@mail.tau.ac.il>
2024-09-16 20:49:15 +0000

    final

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,6 @@ def login():
             st.session_state.eval_comparisons = pd.DataFrame(st.session_state.eval_connection.read(worksheet="comparisons", ttl=1))
             st.session_state.num_evaluations = min(100, len(st.session_state.eval_comparisons))
             last_response_id = pd.DataFrame(st.session_state.eval_connection.read(worksheet="evaluations", ttl=1))['response_id'].max()
-            st.write(last_response_id)
             if last_response_id is np.nan:
                 st.session_state.last_response_id = 0
             else:
