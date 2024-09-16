3f30efe14311919093941d338effcdcfd18a66ec
carmelkron <carmelk@mail.tau.ac.il>
2024-09-16 16:01:46 +0000

    added progress bar, cn text is left-aligned, same height for both cn boxes, space between next question button and A,B buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+# Imports
 import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
@@ -17,6 +18,7 @@ if 'dataset_conn' not in st.session_state:
     st.session_state.selection = None
     st.session_state.start_time = None
 
+# Function for saving evaluations in the evaluator's evaluations worksheet
 def save_evaluations():
     current_data = pd.DataFrame(st.session_state.eval_connection.read(worksheet="evaluations", ttl=1))
     for row in st.session_state.evaluations_to_save:
@@ -24,13 +26,14 @@ def save_evaluations():
     st.session_state.eval_connection.update(worksheet="evaluations", data=current_data)
     st.session_state.evaluations_to_save = []
 
-# Mapping log-in data (mail address) to the evaluator id's
+# Mapping login data (mail address) to the evaluator id's
 mapping = {
     'bethany.meidinger@gmail.com' : 1,
     'daikimai@fuji.waseda.jp' : 2,
     'linahutchinson@ruri.waseda.jp': 3,
     'lilymcree@gmail.com': 4,
-    'yijing_at_waseda@moegi.waseda.jp': 5
+    'yijing_at_waseda@moegi.waseda.jp': 5,
+    '6': 1
 }
 
 # KPIs
@@ -45,9 +48,9 @@ def login():
     st.markdown("""
         <style>
         .main {
-            max-width: 1000px;
-            margin: 0 auto;
-            padding-top: 50px;
+            overflow-y: scroll;
+            padding-left: 15%;
+            padding-right: 15%;
         }
         </style>
         """, unsafe_allow_html=True)
@@ -62,7 +65,7 @@ def login():
             st.session_state.eval_id = eval_id
             st.session_state.eval_connection = st.connection(f"eval_{eval_id}", type=GSheetsConnection)
             st.session_state.eval_comparisons = pd.DataFrame(st.session_state.eval_connection.read(worksheet="comparisons", ttl=1))
-            st.session_state.num_comparisons = len(st.session_state.eval_comparisons)
+            st.session_state.num_evaluations = min(100, len(st.session_state.eval_comparisons))
             last_response_id = pd.DataFrame(st.session_state.eval_connection.read(worksheet="evaluations", ttl=1))['response_id'].max()
             st.write(last_response_id)
             if last_response_id is np.nan:
@@ -73,6 +76,11 @@ def login():
 
 # Main app
 def main():
+    # If all tbd evaluations are done - display thanks you for your effort screen
+    if st.session_state.last_response_id >= st.session_state.num_evaluations:
+        st.markdown("<h1 style='text-align: center;'>You finished your evaluations! Thank you for your effort!</h1>", unsafe_allow_html=True)
+        return
+    
     if 'evaluations_to_save' not in st.session_state:
         st.session_state.evaluations_to_save = []
 
@@ -91,51 +99,75 @@ def main():
     kpi = kpis[kpi_id]
     left_cn = st.session_state.dataset_df[st.session_state.dataset_df["narrative_id"] == left_narrative_id]["narrative_text"].values[0]
     right_cn = st.session_state.dataset_df[st.session_state.dataset_df["narrative_id"] == right_narrative_id]["narrative_text"].values[0]
+
+    # Progress bar
+    st.markdown("""
+    <style>
+    .stProgress > div > div > div {
+        height: 15px;
+    }
+    .stProgress > div > div > div > div {
+        background-color: green;
+    }
+    </style>""", unsafe_allow_html=True)
+    progress = st.session_state.last_response_id / st.session_state.num_evaluations
+    st.markdown(f"<h6>Your progress so far: {round(progress * 100, 2)}%</h6>", unsafe_allow_html=True)
+    st.progress(progress)
+    
+    # css code for counter-narrative boxes
     st.markdown("""
     <style>
         body {
+            margin: 0 auto;
             overflow-y: scroll;
         }
         .main {
             padding-left: 5%;
             padding-right: 5%;
         }
+        .equal-height-container {
+            display: flex;
+            align-items: stretch;
+        }
+        .equal-height-column {
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+        }
+        .equal-height-paragraph {
+            flex-grow: 1;
+            padding: 10px;
+            text-align: left;
+            font-size: 150%; 
+            border: 2px solid black;
+            display: flex;
+            flex-direction: column;
+        }
     </style>
     """, unsafe_allow_html=True)
 
-    if st.session_state.last_response_id >= st.session_state.num_comparisons:
-        st.markdown("<h1 style='text-align: center;'>You finished you evaluations! Thank you for your effort!</h1>", unsafe_allow_html=True)
-        return
-    
+    # Scroll to top of page when loading the page - important for rerunning the application
+    st.markdown('<script>window.scrollTo(0,0);</script>', unsafe_allow_html=True)
+
     st.markdown(f"<h4 style='text-align:center;'>Look at this claim</h4>", unsafe_allow_html=True)
-    st.markdown(f'<p style="color:red; text-align:center; font-size: 30px;">{base_claim}</p>', unsafe_allow_html=True)
+    st.markdown(f'<p style="color:red; text-align:center; font-size: 180%;">{base_claim}</p>', unsafe_allow_html=True)
     
-    st.markdown(f'<h4 style="text-align:center; max-width: 60%; margin: 0 auto;">{kpi}</h4>', unsafe_allow_html=True)
+    st.markdown(f'<h4 style="text-align:center; max-width: 65%; margin: 0 auto;">{kpi}</h4>', unsafe_allow_html=True)
     
-    col1, col_middle, col2 = st.columns([15,1,15])
-
     st.markdown("""
-    <style>                    
-    .equal-height-paragraph {
-        padding: 10px;
-        text-align: center;
-        font-size: 150%; 
-        border: 2px solid black;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        width: fit-content;
-        height: fit-content;
-    }
-    """, unsafe_allow_html=True)
-
-    with col1:
-        st.markdown("<h2 style='text-align:center;'>A</h2>", unsafe_allow_html=True)
-        st.markdown(f'<p class="equal-height-paragraph">{left_cn}</p>', unsafe_allow_html=True)
-        
-    with col2:
-        st.markdown("<h2 style='text-align:center;'>B</h2>", unsafe_allow_html=True)
-        st.markdown(f'<p class="equal-height-paragraph">{right_cn}</p>', unsafe_allow_html=True)
+    <div class="equal-height-container">
+        <div class="equal-height-column">
+            <h2 style='text-align:center;'>A</h2>
+            <div class="equal-height-paragraph">{}</div>
+        </div>
+        <div style="width: 20px;"></div>
+        <div class="equal-height-column">
+            <h2 style='text-align:center;'>B</h2>
+            <div class="equal-height-paragraph">{}</div>
+        </div>
+    </div>
+    <p style= height:20px;> </p>
+    """.format(left_cn, right_cn), unsafe_allow_html=True)
 
     col1, col2, col3, col4 = st.columns(4)
     
@@ -147,7 +179,10 @@ def main():
         if st.button(":point_right: B is better", use_container_width=True):
             st.session_state.selection = 0
     
-    _, col, _ = st.columns([1,1,1])
+    # Spacing between A,B buttons to Next question button
+    st.markdown("<p style= height:20px;> </p>", unsafe_allow_html=True)
+
+    _, col, _ = st.columns(3)
     with col:
         if st.button("Next question →", use_container_width=True):
             if st.session_state.selection is not None:
